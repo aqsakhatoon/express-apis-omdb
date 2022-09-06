c9bbdd5f794185bdf0d42fdad041cd9ef62e2b0b
aqsakhatoon <khanabrar_02@yahoo.com>
2022-09-06 11:31:07 -0500

    hide api key

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const axios = require('axios')
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -14,11 +15,24 @@ app.use(ejsLayouts);
 
 // Adds some logging to each request
 app.use(require('morgan')('dev'));
+console.log('process.env:', processs.env.API_KEY)
+// http://www.omdbapi.com/?i=tt3896198&apikey=6abed851
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  res.render('index.ejs');
 });
+
+app.get('/search', async (req, res) => {
+  const url = `http://www.omdbapi.com/?i=tt3896198&apikey=6abed851${process.env.API_KEY}`;
+  axios.get(url).then(response =>
+    {
+      res.render("results.ejs", {
+        searchResults: response.data.results,
+        searchInput: req.query.userInput
+      });
+    })
+})
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
